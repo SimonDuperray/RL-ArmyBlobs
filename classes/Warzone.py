119afd42185cae64c115ef1fe093d8ebb14eab60
Simon Duperray <64204457+SimonDuperray@users.noreply.github.com>
2022-05-25 08:49:22 +0200

    :ambulance: algo but not showing

--- classes/Warzone.py
+++ classes/Warzone.py
@@ -20,6 +20,11 @@ class Warzone:
       self.targets = []
       self.enemies = []
       self.rm = None
+      self.q_values = np.zeros((self.size, self.size, 4))
+      self.actions = ['up', 'right', 'down', 'left']
+      self.epsilon = 0.9
+      self.discount_factor = 0.9
+      self.learning_rate = 0.9
 
    def __str__(self):
       return f"""
@@ -129,20 +134,91 @@ class Warzone:
       return False
 
    def get_rewards_map(self):
-      rm = np.full((self.size+1, self.size+1), -1)
+      rm = np.full((self.size+1, self.size+1), -1.)
       pts_in_walls = self.__points_in_walls
       for pt_in_wall in pts_in_walls:
-         rm[self.size-pt_in_wall[1], pt_in_wall[0]] = -100
+         rm[self.size-pt_in_wall[1], pt_in_wall[0]] = -100.
       # targets
       for target in self.targets:
-         rm[self.size-target.y, target.x] = 100
+         rm[self.size-target.y, target.x] = 100.
       # enemies
       for enemy in self.enemies:
-         rm[self.size-enemy['blob_enemy'].y, enemy['blob_enemy'].x] = -50
+         rm[self.size-enemy['blob_enemy'].y, enemy['blob_enemy'].x] = -50.
          for r in enemy['ranged_points']:
-            rm[self.size-r[1], r[0]] = -20
+            rm[self.size-r[1], r[0]] = -20.
       self.rm = rm
       return self.rm
+
+   def is_terminal_state(self, current_row_index, current_col_index):
+      if self.rm[current_col_index, current_row_index] == -1.:
+         return False
+      else:
+         return True
+
+   def get_starting_location(self):
+      current_row_index = np.random.randint(self.size)
+      current_column_index = np.random.randint(self.size)
+      while self.is_terminal_state(current_row_index, current_column_index):
+         current_row_index = np.random.randint(self.size)
+         current_column_index = np.random.randint(self.size)
+      return current_row_index, current_column_index
+
+   def get_next_action(self, current_row_index, current_column_index, epsilon):
+      if np.random.random() < epsilon:
+         return np.argmax(self.q_values[current_row_index, current_column_index])
+      else:
+         return np.random.randint(4)
+
+   def get_next_location(self, current_row_index, current_column_index, action_index):
+      new_row_index = current_row_index
+      new_column_index = current_column_index
+      if self.actions[action_index] == 'up' and current_row_index > 0:
+         new_row_index -= 1
+      elif self.actions[action_index] == 'right' and current_column_index < self.size - 1:
+         new_column_index += 1
+      elif self.actions[action_index] == 'down' and current_row_index < self.size - 1:
+         new_row_index += 1
+      elif self.actions[action_index] == 'left' and current_column_index > 0:
+         new_column_index -= 1
+      return new_row_index, new_column_index
+
+   def get_shortest_path(self, start_row_index, start_column_index):
+      #return immediately if this is an invalid starting location
+      if self.is_terminal_state(start_row_index, start_column_index):
+         return []
+      else: #if this is a 'legal' starting location
+         current_row_index, current_column_index = start_row_index, start_column_index
+         shortest_path = []
+         shortest_path.append([current_row_index, current_column_index])
+         #continue moving along the path until we reach the goal (i.e., the item packaging location)
+         while not self.is_terminal_state(current_row_index, current_column_index):
+            #get the best action to take
+            action_index = self.get_next_action(current_row_index, current_column_index, 1.)
+            #move to the next location on the path, and add the new location to the list
+            current_row_index, current_column_index = self.get_next_location(current_row_index, current_column_index, action_index)
+            shortest_path.append([current_row_index, current_column_index])
+         return shortest_path
+
+   def train(self):
+      for episode in range(1000):
+         #get the starting location for this episode
+         row_index, column_index = self.get_starting_location()
+         #continue taking actions (i.e., moving) until we reach a terminal state
+         #(i.e., until we reach the item packaging area or crash into an item storage location)
+         while not self.is_terminal_state(row_index, column_index):
+            #choose which action to take (i.e., where to move next)
+            action_index = self.get_next_action(row_index, column_index, self.epsilon)
+            #perform the chosen action, and transition to the next state (i.e., move to the next location)
+            old_row_index, old_column_index = row_index, column_index #store the old row and column indexes
+            row_index, column_index = self.get_next_location(row_index, column_index, action_index)
+            #receive the reward for moving to the new state, and calculate the temporal difference
+            reward = self.rm[row_index, column_index]
+            old_q_value = self.q_values[old_row_index, old_column_index, action_index]
+            temporal_difference = reward + (self.discount_factor * np.max(self.q_values[row_index, column_index])) - old_q_value
+            #update the Q-value for the previous state and action pair
+            new_q_value = old_q_value + (self.learning_rate * temporal_difference)
+            self.q_values[old_row_index, old_column_index, action_index] = new_q_value
+      print('Training complete!')
 
    def run(self):
       plt.show()
--- main.py
+++ main.py
@@ -5,6 +5,8 @@ from classes.BlobTypes import BlobTypes
 import matplotlib.pyplot as plt
 
 ENV_SIZE = 20
+ITERATIONS = 10
+SHOW_EVERY = 2
 
 warzone = Warzone(size=ENV_SIZE)
 warzone.build_wall((0, 14), 5, 6)
@@ -14,9 +16,11 @@ warzone.add_agent(Blob(ENV_SIZE, BlobTypes.PLAYER, x=1, y=1))
 warzone.add_target(Blob(ENV_SIZE, BlobTypes.TARGET, x=18, y=18))
 warzone.add_enemy(Blob(ENV_SIZE, BlobTypes.ENEMY, range=4, x=10, y=10))
 warzone.add_enemy(Blob(ENV_SIZE, BlobTypes.ENEMY, range=2, x=17, y=10))
-rm = warzone.get_rewards_map()
-print(rm)
-warzone.run()
-plt.imshow(rm, cmap='hot', interpolation='nearest')
-plt.show()
+for it in range(ITERATIONS):
+   warzone.train()
+   if it % SHOW_EVERY == 0:
+      warzone.run()
+warzone.train()
+# plt.imshow(rm, cmap='hot', interpolation='nearest')
+# plt.show()
 # print(warzone)
