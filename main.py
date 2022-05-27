71ac9939b6acaff35d0afcbb3863083a492512d4
Simon Duperray <64204457+SimonDuperray@users.noreply.github.com>
2022-05-27 22:38:04 +0200

    :bookmark: ok in 2d

--- main.py
+++ main.py
@@ -1,79 +1,84 @@
-from classes.Blob import Blob
-from classes.Warzone import Warzone
-from classes.BlobTypes import BlobTypes
-import matplotlib.pyplot as plt, numpy as np
+from calendar import c
+import numpy as np, matplotlib.pyplot as plt
+plt.style.use('ggplot')
 
-ENV_SIZE = 20
-EPISODES = 10
-SHOW_EVERY = 2
-EPSILON = 0.9
-EPS_DECAY = 1e-3
-EPISODES = 30000
-SHOW_EVERY = 3000
-EPS_DECAY = 0.9998
-LEARNING_RATE = 0.1
-DISCOUNT = 0.95
-ENEMY_PENALTY = 300
-TARGET_REWARD = 100
-MOOVE_PENALTY = 1
-# ===
-BOARD_COLS = 5
+# global variables
 BOARD_ROWS = 5
-ENV_SIZE = 5
-
-WIN_STATE = (5, 5)
-ENEMY_STATE = (4, 4)
-START = (1, 1)
+BOARD_COLS = 5
+TARGET_STATE = (4, 4)
+LOSE_STATES = [(1, 3)]
+START = (4, 0)
+WALLS = [
+   ((1, 2), 1, 2)
+]
 DETERMINISTIC = True
 
-# WALLS = [(2, 2), (3, 3), (4, 4)]
-TARGET_REWARD = 100
-MOOVE_PENALTY = 1
-ENEMY_PENALTY = 50
+
+def get_points_in_wall(walls):
+   pts = []
+   for wall in walls:
+      x = wall[0][0]
+      y = wall[0][1]
+      w = wall[1]
+      h = wall[2]
+      for xi in range(x, x+w+1):
+         for yi in range(y, y+h+1):
+            pts.append((yi, xi))
+   return list(dict.fromkeys(pts))
 
 class State:
    def __init__(self, state=START):
-      self.board = np.full([BOARD_ROWS+1, BOARD_COLS+1], -MOOVE_PENALTY)
-      self.board[ENEMY_STATE[0], ENEMY_STATE[1]] = -ENEMY_PENALTY
-      self.board[WIN_STATE[0], WIN_STATE[1]] = TARGET_REWARD
+      self.board = np.zeros([BOARD_ROWS, BOARD_COLS])
+      for wall in WALLS:
+         self.board[wall[0], wall[1]]=-1
       self.state = state
       self.isEnd = False
       self.determine = DETERMINISTIC
+      self.impossible_points = []
 
-   def give_reward(self):
-      if self.state == WIN_STATE:
-         return TARGET_REWARD
-      elif self.state == ENEMY_STATE:
-         return ENEMY_PENALTY
+   def giveReward(self):
+      if self.state == TARGET_STATE:
+         return 1
+      elif self.state in LOSE_STATES:
+         return -1
       else:
-         return -MOOVE_PENALTY
+         return 0
 
-   def is_end(self):
-      if(self.state==WIN_STATE or self.state==ENEMY_STATE):
+   def isEndFunc(self):
+      if (self.state == TARGET_STATE) or (self.state in LOSE_STATES):
          self.isEnd = True
 
-   def next_position(self, action):
-      if action=="up":
-         next_state = (self.state[0]-1, self.state[1])
-      elif action=="down":
-         next_state = (self.state[0]+1, self.state[1])
-      elif action=="left":
-         next_state = (self.state[0], self.state[1]-1)
-      else:
-         next_state = (self.state[0], self.state[1]+1)
-      
-      if (next_state[0]>=0) and (next_state[0]<=ENV_SIZE-1):
-         if (next_state[1]>=0) and (next_state[1]<=ENV_SIZE-1):
-            if next_state != ENEMY_STATE:
-               return next_state
-      return self.state
+   def nxtPosition(self, action):
+      """
+      action: up, down, left, right
+      -------------
+      0 | 1 | 2| 3|
+      1 |
+      2 |
+      return next position
+      """
+      if self.determine:
+         if action == "up":
+            nxtState = (self.state[0] - 1, self.state[1])
+         elif action == "down":
+            nxtState = (self.state[0] + 1, self.state[1])
+         elif action == "left":
+            nxtState = (self.state[0], self.state[1] - 1)
+         else:
+            nxtState = (self.state[0], self.state[1] + 1)
+         # if next state legal
+         if (nxtState[0] >= 0) and (nxtState[0] <= (BOARD_ROWS)):
+            if (nxtState[1] >= 0) and (nxtState[1] <= (BOARD_COLS)):
+               if nxtState not in get_points_in_wall(WALLS):
+                  return nxtState
+         return self.state
 
-   def show_board(self):
+   def showBoard(self):
       self.board[self.state] = 1
-      for i in range(0, ENV_SIZE):
-         print('-----------------')
+      for i in range(0, BOARD_ROWS):
+         print('-----------------------')
          out = '| '
-         for j in range(0, ENV_SIZE):
+         for j in range(0, BOARD_COLS):
             if self.board[i, j] == 1:
                token = '*'
             if self.board[i, j] == -1:
@@ -82,35 +87,45 @@ class State:
                token = '0'
             out += token + ' | '
          print(out)
-      print('-----------------')
+      print('-----------------------')
+
+
+# Agent of player
 
 class Agent:
+
    def __init__(self):
       self.states = []
-      self.actions = ['up', 'down', 'left', 'right']
+      self.actions = ["up", "down", "left", "right"]
       self.State = State()
       self.lr = 0.2
       self.exp_rate = 0.3
-      self.state_values = {}
-      for i in range(ENV_SIZE):
-         for j in range(ENV_SIZE):
-            self.state_values[(i, j)] = 0
 
-   def choose_action(self):
-      max_next_reward = 0
+      # initial state reward
+      self.state_values = {}
+      for i in range(BOARD_ROWS+1):
+         for j in range(BOARD_COLS+1):
+            self.state_values[(i, j)] = 0  # set initial value to 0
+
+   def chooseAction(self):
+      # choose action with most expected value
+      mx_nxt_reward = 0
       action = ""
-      if np.random.uniform(0, 1)<=self.exp_rate:
+
+      if np.random.uniform(0, 1) <= self.exp_rate:
          action = np.random.choice(self.actions)
       else:
+         # greedy action
          for a in self.actions:
-            next_reward = self.state_values[self.State.next_position(a)]
-            if next_reward>=max_next_reward:
+            # if the action is deterministic
+            nxt_reward = self.state_values[self.State.nxtPosition(a)]
+            if nxt_reward >= mx_nxt_reward:
                action = a
-               max_next_reward = next_reward
+               mx_nxt_reward = nxt_reward
       return action
 
-   def take_action(self, action):
-      position = self.State.next_position(action)
+   def takeAction(self, action):
+      position = self.State.nxtPosition(action)
       return State(state=position)
 
    def reset(self):
@@ -118,66 +133,70 @@ class Agent:
       self.State = State()
 
    def play(self, rounds=10):
-      i=0
-      while i<rounds:
-         if self.State.is_end:
-            reward = self.State.give_reward()
-            self.state_values[self.State.state] = reward
-            print(f"Game Ended - Reward: {reward}")
+      i = 0
+      while i < rounds+1:
+         # to the end of game back propagate reward
+         if self.State.isEnd:
+            # back propagate
+            reward = self.State.giveReward()
+            # explicitly assign end state to reward values
+            self.state_values[self.State.state] = reward  # this is optional
+            # print("Game End Reward", reward)
             for s in reversed(self.states):
-               reward = self.state_values[s]+self.lr*(reward-self.state_values[s])
+               reward = self.state_values[s] + self.lr * (reward - self.state_values[s])
                self.state_values[s] = round(reward, 3)
             self.reset()
-            i+=1
+            i += 1
          else:
-            action = self.choose_action()
-            self.states.append(self.State.next_position(action))
-            print(f"current position {self.State.state} - action: {action}")
-            self.State = self.take_action(action)
-            self.State.is_end()
-            print(f"next state: {self.State.state}")
-            print('-----------------')
-            # plot env
-            plt.suptitle(f"Round {i}")
-            plt.xlim(-1, ENV_SIZE+1)
-            plt.ylim(-1, ENV_SIZE+1)
-            plt.plot([0, 0], [ENV_SIZE, 0], color="black", linewidth=2)
-            plt.plot([0, ENV_SIZE], [ENV_SIZE, ENV_SIZE], color="black", linewidth=2)
-            plt.plot([ENV_SIZE, ENV_SIZE], [ENV_SIZE, 0], color="black", linewidth=2)
-            plt.plot([ENV_SIZE, 0], [0, 0], color="black", linewidth=2)
-            plt.scatter(self.State.state[1], self.State.state[0], color='b')
-            plt.scatter(WIN_STATE[0], WIN_STATE[1], color='green')
-            plt.Scatter(ENEMY_STATE[0], ENEMY_STATE[1], color='red', marker='x')
-            plt.pause(0.1)
-            plt.clf()
+            action = self.chooseAction()
+            # append trace
+            self.states.append(self.State.nxtPosition(action))
+            # print("current position {} action {}".format(self.State.state, action))
+            # by taking the action, it reaches the next state
+            self.State = self.takeAction(action)
+            # mark is end
+            self.State.isEndFunc()
+            # print("nxt state", self.State.state)
+            # print("---------------------")
+            if i%2500==0:
+               plt.suptitle(f"Round {i}/{rounds}")
+               plt.xlim(-1, BOARD_COLS+1)
+               plt.ylim(-1, BOARD_ROWS+1)
+               # plot borders
+               plt.plot([0, BOARD_COLS], [0, 0], color='black')
+               plt.plot([0, 0], [0, BOARD_ROWS], color='black')
+               plt.plot([BOARD_COLS, BOARD_COLS], [0, BOARD_ROWS], color='black')
+               plt.plot([0, BOARD_COLS], [BOARD_ROWS, BOARD_ROWS], color='black')
+               for loose in LOSE_STATES:
+                  plt.scatter(loose[1], loose[0], marker='x', color='red', label='loose')
+               for wall in WALLS:
+                  # plt.scatter(wall[1], wall[0], marker='x', color='black', label='wall')
+                  x = wall[0][0]
+                  y = wall[0][1]
+                  w = wall[1]
+                  h = wall[2]
+                  plt.plot([x, x], [y, y+h], color='black')
+                  plt.plot([x, x+w], [y, y], color='black')
+                  plt.plot([x, x+w], [y+h, y+h], color='black')
+                  plt.plot([x+w, x+w], [y, y+h], color='black')
+               plt.scatter(self.State.state[1], self.State.state[0], color='blue', label="agent")
+               plt.scatter(TARGET_STATE[1], TARGET_STATE[0], color='green', marker='<', label="target")
+               plt.legend(loc='upper left')
+               plt.gca().invert_yaxis()
+               plt.pause(.1)
+               plt.clf()
 
-   def show_values(self):
-      for i in range(0, ENV_SIZE):
-         print('----------------------------------')
+   def showValues(self):
+      for i in range(0, BOARD_ROWS+1):
+         print('-------------------------------------------------------')
          out = '| '
-         for j in range(0, ENV_SIZE):
-            out += str(self.state_values[(i, j)])+' | '
+         for j in range(0, BOARD_COLS+1):
+            out += str(self.state_values[(i, j)]).ljust(6) + ' | '
          print(out)
-      print('----------------------------------')
+      print('-------------------------------------------------------')
 
 
 if __name__ == "__main__":
-   agent = Agent()
-   agent.play(50)
-   print(agent.show_values())
-
-# epsilon = 0.9
-
-# warzone = Warzone(size=ENV_SIZE)
-# warzone.build_wall((0, 14), 5, 6)
-# warzone.build_wall((5, 9), 2, 5)
-# warzone.build_wall((15, 0), 5, 5)
-# warzone.add_agent(Blob(ENV_SIZE, BlobTypes.PLAYER, x=1, y=1))
-# warzone.add_target(Blob(ENV_SIZE, BlobTypes.TARGET, x=18, y=18))
-# warzone.add_enemy(Blob(ENV_SIZE, BlobTypes.ENEMY, range=4, x=10, y=10))
-# warzone.add_enemy(Blob(ENV_SIZE, BlobTypes.ENEMY, range=2, x=17, y=10))
-# warzone.run()
-# warzone.train()
-# plt.imshow(warzone.get_rewards_map(), cmap='hot', interpolation='nearest')
-# plt.show()
-# print(warzone)
+   ag = Agent()
+   ag.play(10000)
+   print(ag.showValues())
--- test.py
+++ test.py
@@ -1,172 +1,48 @@
-import numpy as np, matplotlib.pyplot as plt
-plt.style.use('ggplot')
 
-# global variables
-BOARD_ROWS = 5
-BOARD_COLS = 5
-WIN_STATE = (0, 4)
-LOSE_STATES = [(1, 3)]
-START = (2, 0)
-WALLS = [(0, 0)]
-DETERMINISTIC = True
+# from mpl_toolkits import mplot3d
+# import numpy as np
+# import matplotlib.pyplot as plt
+  
+# fig = plt.figure(figsize = (8,8))
+# ax = plt.axes(projection = '3d')
+  
+# x = [i for i in range(0,10)]
 
+# for xi in x:
+#    ax.scatter(x, x, color='red')
+#    plt.pause(.1)
+#    plt.clf()
+#    ax
 
-class State:
-   def __init__(self, state=START):
-      self.board = np.zeros([BOARD_ROWS, BOARD_COLS])
-      for wall in WALLS:
-         self.board[wall[0], wall[1]]=-1
-      self.state = state
-      self.isEnd = False
-      self.determine = DETERMINISTIC
+import matplotlib.pyplot as plt
+import numpy as np
 
-   def giveReward(self):
-      if self.state == WIN_STATE:
-         return 1
-      elif self.state in LOSE_STATES:
-         return -1
-      else:
-         return 0
-
-   def isEndFunc(self):
-      if (self.state == WIN_STATE) or (self.state in LOSE_STATES):
-         self.isEnd = True
-
-   def nxtPosition(self, action):
-      """
-      action: up, down, left, right
-      -------------
-      0 | 1 | 2| 3|
-      1 |
-      2 |
-      return next position
-      """
-      if self.determine:
-         if action == "up":
-            nxtState = (self.state[0] - 1, self.state[1])
-         elif action == "down":
-            nxtState = (self.state[0] + 1, self.state[1])
-         elif action == "left":
-            nxtState = (self.state[0], self.state[1] - 1)
-         else:
-            nxtState = (self.state[0], self.state[1] + 1)
-         # if next state legal
-         if (nxtState[0] >= 0) and (nxtState[0] <= (BOARD_ROWS -1)):
-            if (nxtState[1] >= 0) and (nxtState[1] <= (BOARD_COLS -1)):
-               if nxtState not in WALLS:
-                  return nxtState
-         return self.state
-
-   def showBoard(self):
-      self.board[self.state] = 1
-      for i in range(0, BOARD_ROWS):
-         print('-----------------------')
-         out = '| '
-         for j in range(0, BOARD_COLS):
-            if self.board[i, j] == 1:
-               token = '*'
-            if self.board[i, j] == -1:
-               token = 'z'
-            if self.board[i, j] == 0:
-               token = '0'
-            out += token + ' | '
-         print(out)
-      print('-----------------------')
+# Fixing random state for reproducibility
+np.random.seed(19680801)
 
 
-# Agent of player
+def randrange(n, vmin, vmax):
+    """
+    Helper function to make an array of random numbers having shape (n, )
+    with each number distributed Uniform(vmin, vmax).
+    """
+    return (vmax - vmin)*np.random.rand(n) + vmin
 
-class Agent:
+fig = plt.figure()
+ax = fig.add_subplot(projection='3d')
 
-   def __init__(self):
-      self.states = []
-      self.actions = ["up", "down", "left", "right"]
-      self.State = State()
-      self.lr = 0.2
-      self.exp_rate = 0.3
+n = 100
 
-      # initial state reward
-      self.state_values = {}
-      for i in range(BOARD_ROWS):
-         for j in range(BOARD_COLS):
-            self.state_values[(i, j)] = 0  # set initial value to 0
+# For each set of style and range settings, plot n random points in the box
+# defined by x in [23, 32], y in [0, 100], z in [zlow, zhigh].
+for m, zlow, zhigh in [('o', -50, -25), ('^', -30, -5)]:
+    xs = randrange(n, 23, 32)
+    ys = randrange(n, 0, 100)
+    zs = randrange(n, zlow, zhigh)
+    ax.scatter(xs, ys, zs, marker=m)
 
-   def chooseAction(self):
-      # choose action with most expected value
-      mx_nxt_reward = 0
-      action = ""
+ax.set_xlabel('X Label')
+ax.set_ylabel('Y Label')
+ax.set_zlabel('Z Label')
 
-      if np.random.uniform(0, 1) <= self.exp_rate:
-         action = np.random.choice(self.actions)
-      else:
-         # greedy action
-         for a in self.actions:
-            # if the action is deterministic
-            nxt_reward = self.state_values[self.State.nxtPosition(a)]
-            if nxt_reward >= mx_nxt_reward:
-               action = a
-               mx_nxt_reward = nxt_reward
-      return action
-
-   def takeAction(self, action):
-      position = self.State.nxtPosition(action)
-      return State(state=position)
-
-   def reset(self):
-      self.states = []
-      self.State = State()
-
-   def play(self, rounds=10):
-      i = 0
-      while i < rounds+1:
-         # to the end of game back propagate reward
-         if self.State.isEnd:
-            # back propagate
-            reward = self.State.giveReward()
-            # explicitly assign end state to reward values
-            self.state_values[self.State.state] = reward  # this is optional
-            print("Game End Reward", reward)
-            for s in reversed(self.states):
-               reward = self.state_values[s] + self.lr * (reward - self.state_values[s])
-               self.state_values[s] = round(reward, 3)
-            self.reset()
-            i += 1
-         else:
-            action = self.chooseAction()
-            # append trace
-            self.states.append(self.State.nxtPosition(action))
-            print("current position {} action {}".format(self.State.state, action))
-            # by taking the action, it reaches the next state
-            self.State = self.takeAction(action)
-            # mark is end
-            self.State.isEndFunc()
-            print("nxt state", self.State.state)
-            print("---------------------")
-            if i%5==0:
-               plt.suptitle(f"Round {i}/{rounds}")
-               plt.xlim(-1, BOARD_COLS+1)
-               plt.ylim(-1, BOARD_ROWS+1)
-               for loose in LOSE_STATES:
-                  plt.scatter(loose[1], loose[0], marker='x', color='red', label='loose')
-               for wall in WALLS:
-                  plt.scatter(wall[1], wall[0], marker='x', color='black', label='wall')
-               plt.scatter(self.State.state[1], self.State.state[0], color='blue')
-               plt.scatter(WIN_STATE[1], WIN_STATE[0], color='green', marker='<')
-               plt.legend(loc='upper left')
-               plt.pause(.1)
-               plt.clf()
-
-   def showValues(self):
-      for i in range(0, BOARD_ROWS):
-         print('----------------------------------------------')
-         out = '| '
-         for j in range(0, BOARD_COLS):
-            out += str(self.state_values[(i, j)]).ljust(6) + ' | '
-         print(out)
-      print('----------------------------------------------')
-
-
-if __name__ == "__main__":
-   ag = Agent()
-   ag.play(50)
-   print(ag.showValues())
+plt.show()
