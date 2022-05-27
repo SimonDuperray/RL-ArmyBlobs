02aa71c4aa4b0bc5fb0696eaeb77203010ea63a8
Simon Duperray <64204457+SimonDuperray@users.noreply.github.com>
2022-05-28 00:52:15 +0200

    :sparkles: 3d ok+agent path+policy table dyn

--- main.py
+++ main.py
@@ -1,15 +1,16 @@
 from calendar import c
-import numpy as np, matplotlib.pyplot as plt
+import numpy as np, matplotlib.pyplot as plt, time
 plt.style.use('ggplot')
 
 # global variables
-BOARD_ROWS = 5
-BOARD_COLS = 5
+BOARD_ROWS = 10
+BOARD_COLS = 10
 TARGET_STATE = (4, 4)
 LOSE_STATES = [(1, 3)]
 START = (4, 0)
 WALLS = [
-   ((1, 2), 1, 2)
+   ((1, 2), 1, 2),
+   ((5, 5), 2, 2)
 ]
 DETERMINISTIC = True
 
@@ -34,7 +35,6 @@ class State:
       self.state = state
       self.isEnd = False
       self.determine = DETERMINISTIC
-      self.impossible_points = []
 
    def giveReward(self):
       if self.state == TARGET_STATE:
@@ -100,7 +100,7 @@ class Agent:
       self.State = State()
       self.lr = 0.2
       self.exp_rate = 0.3
-
+      self.cumulative_mooves = 0
       # initial state reward
       self.state_values = {}
       for i in range(BOARD_ROWS+1):
@@ -126,7 +126,11 @@ class Agent:
 
    def takeAction(self, action):
       position = self.State.nxtPosition(action)
+      self.cumulative_mooves+=1
       return State(state=position)
+
+   def is_tired(self):
+      return self.cumulative_mooves==200
 
    def reset(self):
       self.states = []
@@ -137,6 +141,7 @@ class Agent:
       ax = fig.add_subplot(111, projection='3d')
       i = 0
       while i < rounds+1:
+         axs, ays, azs = [], [], []
          # to the end of game back propagate reward
          if self.State.isEnd:
             # back propagate
@@ -150,70 +155,85 @@ class Agent:
             self.reset()
             i += 1
          else:
+            # for j in range(200):
             action = self.chooseAction()
             # append trace
             self.states.append(self.State.nxtPosition(action))
             # print("current position {} action {}".format(self.State.state, action))
             # by taking the action, it reaches the next state
             self.State = self.takeAction(action)
+            axs.append(self.State.state[1])
+            ays.append(self.State.state[0])
+            azs.append(0)
             # mark is end
             self.State.isEndFunc()
+            if self.is_tired():
+               self.cumulative_mooves = 0
+               i+=1
+            else:
+            # if self.State.giveReward()==1 or self.State.giveReward()==-1:
+            #    break
             # print("nxt state", self.State.state)
             # print("---------------------")
-            # if i%2500==0:
-            if i==rounds-1:
-               fig.suptitle(f"Round {i}/{rounds}")
-               plt.xlim(-1, BOARD_COLS+1)
-               plt.ylim(-1, BOARD_ROWS+1)
-               ax.set_zlim(0,3)
-               # plot borders
-               ax.plot([0, BOARD_COLS], [0, 0], color='black')
-               ax.plot([0, 0], [0, BOARD_ROWS], color='black')
-               ax.plot([BOARD_COLS, BOARD_COLS], [0, BOARD_ROWS], color='black')
-               ax.plot([0, BOARD_COLS], [BOARD_ROWS, BOARD_ROWS], color='black')
-               for loose in LOSE_STATES:
-                  ax.scatter(loose[1], loose[0], marker='x', color='red', label='loose')
-               for wall in WALLS:
-                  # plt.scatter(wall[1], wall[0], marker='x', color='black', label='wall')
-                  x = wall[0][0]
-                  y = wall[0][1]
-                  w = wall[1]
-                  h = wall[2]
-                  # down
-                  ax.plot([x, x], [y, y+h], [0,0], color='black')
-                  ax.plot([x, x+w], [y, y], [0,0], color='black')
-                  ax.plot([x, x+w], [y+h, y+h], [0,0], color='black')
-                  ax.plot([x+w, x+w], [y, y+h], [0,0], color='black')
-                  # corners
-                  ax.plot([x, x], [y, y], [0,.5], color='black')
-                  ax.plot([x+w, x+w], [y, y], [0,.5], color='black')
-                  ax.plot([x, x], [y+h, y+h], [0,.5], color='black')
-                  ax.plot([x+w, x+w], [y+h, y+h], [0,.5], color='black')
-                  # top
-                  ax.plot([x, x], [y, y+h], [.5,.5], color='black')
-                  ax.plot([x, x+w], [y, y], [.5,.5], color='black')
-                  ax.plot([x, x+w], [y+h, y+h], [.5,.5], color='black')
-                  ax.plot([x+w, x+w], [y, y+h], [.5,.5], color='black')
-               # ax.scatter(self.State.state[1], self.State.state[0], 0, color='blue', label="agent")
-               ax.plot([self.State.state[1], self.State.state[1]], [self.State.state[0], self.State.state[0]], [0, .2], color='blue', label='agent')
-               ax.scatter(TARGET_STATE[1], TARGET_STATE[0], color='green', marker='<', label="target")
-               # plt.legend(loc='upper left')
-               plt.gca().invert_yaxis()
-               plt.pause(.5)
-      fig.clf()
+               # if i%5000==0:
+               if i==rounds-1:
+                  fig.suptitle(f"Round {i}/{rounds} - Mooves: {self.cumulative_mooves}/200")
+                  plt.xlim(-1, BOARD_COLS+1)
+                  plt.ylim(-1, BOARD_ROWS+1)
+                  ax.set_zlim(0,3)
+                  # plot borders
+                  ax.plot([0, BOARD_COLS], [0, 0], color='black')
+                  ax.plot([0, 0], [0, BOARD_ROWS], color='black')
+                  ax.plot([BOARD_COLS, BOARD_COLS], [0, BOARD_ROWS], color='black')
+                  ax.plot([0, BOARD_COLS], [BOARD_ROWS, BOARD_ROWS], color='black')
+                  for loose in LOSE_STATES:
+                     ax.scatter(loose[1], loose[0], marker='x', color='red', label='loose')
+                  for wall in WALLS:
+                     x = wall[0][0]
+                     y = wall[0][1]
+                     w = wall[1]
+                     h = wall[2]
+                     # down
+                     ax.plot([x, x], [y, y+h], [0,0], color='black', linewidth=.3)
+                     ax.plot([x, x+w], [y, y], [0,0], color='black', linewidth=.3)
+                     ax.plot([x, x+w], [y+h, y+h], [0,0], color='black', linewidth=.3)
+                     ax.plot([x+w, x+w], [y, y+h], [0,0], color='black', linewidth=.3)
+                     # corners
+                     ax.plot([x, x], [y, y], [0,.5], color='black', linewidth=.3)
+                     ax.plot([x+w, x+w], [y, y], [0,.5], color='black', linewidth=.3)
+                     ax.plot([x, x], [y+h, y+h], [0,.5], color='black', linewidth=.3)
+                     ax.plot([x+w, x+w], [y+h, y+h], [0,.5], color='black', linewidth=.3)
+                     # top
+                     ax.plot([x, x], [y, y+h], [.5,.5], color='black', linewidth=.3)
+                     ax.plot([x, x+w], [y, y], [.5,.5], color='black', linewidth=.3)
+                     ax.plot([x, x+w], [y+h, y+h], [.5,.5], color='black', linewidth=.3)
+                     ax.plot([x+w, x+w], [y, y+h], [.5,.5], color='black', linewidth=.3)
+                  # ax.scatter(self.State.state[1], self.State.state[0], 0, color='blue', label="agent")
+                  ax.plot(ays, axs, azs, color='blue', alpha=1, linewidth=1, linestyle="dotted")
+                  ax.plot([ays[-1], ays[-1]], [axs[-1], axs[-1]], [0, .2], color='blue', alpha=1)
+                  # ax.plot([self.State.state[1], self.State.state[1]], [self.State.state[0], self.State.state[0]], [0, .2], color='blue', label='agent')
+                  ax.scatter(TARGET_STATE[1], TARGET_STATE[0], color='green', marker='<', label="target")
+                  # plt.legend(loc='upper left')
+                  plt.gca().invert_yaxis()
+                  plt.draw()
+                  plt.pause(.1)
+                  ax.clear()
+         # fig.clf()
 
 
    def showValues(self):
+      li = ['----------' for _ in range(BOARD_ROWS)]
+      lines = ''.join(li)
       for i in range(0, BOARD_ROWS+1):
-         print('-------------------------------------------------------')
+         print(lines)
          out = '| '
          for j in range(0, BOARD_COLS+1):
             out += str(self.state_values[(i, j)]).ljust(6) + ' | '
          print(out)
-      print('-------------------------------------------------------')
+      print(lines)
 
 
 if __name__ == "__main__":
    ag = Agent()
-   ag.play(10000)
+   ag.play(20000)
    print(ag.showValues())
